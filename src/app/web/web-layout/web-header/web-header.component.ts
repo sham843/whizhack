725ccc0cf760028419259f5755137f65894e44e3
stpl-skishorUI <kishor.shetti@shauryatechnosoft.com>
2022-11-07 20:50:42 +0530

    whizhack redesign

--- src/app/web/web-layout/web-header/web-header.component.ts
+++ src/app/web/web-layout/web-header/web-header.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Component, HostListener, Inject, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-web-header',
@@ -7,9 +8,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WebHeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(@Inject(DOCUMENT) private document: any) { }
 
   ngOnInit(): void {
+  }
+  @HostListener('window:scroll', ['$event'])
+
+  onWindowScroll() {
+      let element = document.querySelector('.navbar') as HTMLElement;
+      if (window.pageYOffset > element.clientHeight) {
+        element.classList.add('navbar-bg');
+      } else {
+        element.classList.remove('navbar-bg');
+      }
+    }
+  openNav(){
+    this.document.getElementById("myNav").style.width = "100%";
+  }
+
+  closeNav(){
+    this.document.getElementById("myNav").style.width = "0%";
   }
 
 }
