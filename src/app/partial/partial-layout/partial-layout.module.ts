a572d03d9a23a1a3dd5ed84d790a1628f497f7ae
stpl-skishorUI <kishor.shetti@shauryatechnosoft.com>
2022-11-16 18:43:29 +0530

    partial component designed

--- src/app/partial/partial-layout/partial-layout.module.ts
+++ src/app/partial/partial-layout/partial-layout.module.ts
@@ -1,23 +1,44 @@
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
-
-import { PartialLayoutRoutingModule } from './partial-layout-routing.module';
-import { PartialLayoutComponent } from './partial-layout.component';
-import { HeaderComponent } from './header/header.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatRippleModule } from '@angular/material/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatSelectModule } from '@angular/material/select';
+import { PartialLayoutRoutes } from './partial-layout.routing';
+import { MaterialModule } from 'src/app/shared/angularMaterialModule/material.module';
 import { FooterComponent } from './footer/footer.component';
-import { SideBarComponent } from './side-bar/side-bar.component';
+import { HeaderComponent } from './header/header.component';
+import { PartialLayoutComponent } from './partial-layout.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+
+
+
+
 
 
 @NgModule({
   declarations: [
     PartialLayoutComponent,
-    HeaderComponent,
     FooterComponent,
-    SideBarComponent
+    HeaderComponent,
+    SidebarComponent
   ],
   imports: [
     CommonModule,
-    PartialLayoutRoutingModule
-  ]
+    RouterModule.forChild(PartialLayoutRoutes),
+    FormsModule, 
+    ReactiveFormsModule,
+    MatButtonModule,
+    MatInputModule,
+    MatRippleModule,
+    MatFormFieldModule,
+    MatTooltipModule,
+    MatSelectModule,
+    MaterialModule
+  ]  
 })
 export class PartialLayoutModule { }
---
+++ src/app/partial/partial-layout/partial-layout.routing.ts
@@ -0,0 +1,5 @@
+import { Routes } from '@angular/router';
+
+export const PartialLayoutRoutes: Routes = [
+  { path: 'dashboard', loadChildren: () => import('../dashboard/dashboard.module').then(m => m.DashboardModule), data: { title: 'Dashboard' } },
+];
