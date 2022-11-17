272a47e1b3de965c56f08aa9ffaa046b5e625169
stpl-skishorUI <kishor.shetti@shauryatechnosoft.com>
2022-11-17 11:57:39 +0530

    blog master page designed

--- src/app/partial/blog-master/blog-master.component.ts
+++ src/app/partial/blog-master/blog-master.component.ts
@@ -1,12 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface PeriodicElement {
+  name: string;
+  position: number;
+  symbol: string;
+}
+const ELEMENT_DATA: PeriodicElement[] = [
+  {position: 1, name: 'Cyber Attacks You Should Know About: Caution Time!',  symbol: 'H'},
+  {position: 2, name: 'Cybersecurity As A Career Option In India: Skills & Scope',  symbol: 'He'},
+  {position: 3, name: 'Future and Present of Hacking', symbol: 'Li'},
+  {position: 4, name: 'AI and the Evolving Threat Landscape',symbol: 'Be'},
+  {position: 5, name: 'Is Absolute Cybersecurity an Utopian Dream?',  symbol: 'B'},
+  {position: 6, name: 'Top Reasons To Join IIT Jodhpur TISC Advanced Certificate Program on Cyber Defense',  symbol: 'C'},
+  {position: 7, name: 'How To Select The Best Place To Learn Cyber Security', symbol: 'N'},
+  {position: 8, name: 'Cyber Security Courses for Beginners to Build a Successful Career in Cyber Security', symbol: 'O'},
+  {position: 9, name: 'Why Cyber Security Training Programs and Certifications are Worth It', symbol: 'F'},
+  {position: 10, name: 'Top Career Prospects After Completing Cyber Security Courses Online',symbol: 'Ne'},
+];
 @Component({
   selector: 'app-blog-master',
   templateUrl: './blog-master.component.html',
   styleUrls: ['./blog-master.component.css']
 })
 export class BlogMasterComponent implements OnInit {
-
+  displayedColumns: string[] = ['position', 'name', 'symbol'];
+  dataSource = ELEMENT_DATA;
   constructor() { }
 
   ngOnInit(): void {
--- src/app/partial/blog-master/blog-master.module.ts
+++ src/app/partial/blog-master/blog-master.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { BlogMasterRoutingModule } from './blog-master-routing.module';
 import { BlogMasterComponent } from './blog-master.component';
+import { MaterialModule } from 'src/app/shared/angularMaterialModule/material.module';
 
 
 @NgModule({
@@ -11,7 +12,8 @@ import { BlogMasterComponent } from './blog-master.component';
   ],
   imports: [
     CommonModule,
-    BlogMasterRoutingModule
+    BlogMasterRoutingModule,
+    MaterialModule
   ]
 })
 export class BlogMasterModule { }
