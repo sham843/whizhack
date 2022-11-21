8386a4a2af3d493331b6d4bec1655e7e5f11bb45
anandramekar-stpl <anand.ramekar@shauryatechnosoft.com>
2022-11-21 18:53:12 +0530

    login page uploaded

--- src/app/web/login/login.component.ts
+++ src/app/web/login/login.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ApiService } from 'src/app/core/services/api.service';
 import { CommonMethodService } from 'src/app/core/services/common-method.service';
+import { FormValidationService } from 'src/app/core/services/form-validation.service';
 
 @Component({
   selector: 'app-login',
@@ -8,14 +12,64 @@ import { CommonMethodService } from 'src/app/core/services/common-method.service
 })
 export class LoginComponent implements OnInit {
   hide = true;
-  constructor( private commonMethodService: CommonMethodService) { }
+  loginForm!: FormGroup;
+  constructor(private commonMethodService: CommonMethodService, private fb: FormBuilder,
+    public validation: FormValidationService, private service: ApiService, private router: Router) { }
 
   ngOnInit(): void {
     this.captcha();
+    this.controlLoginForm();
   }
 
   captcha() {
     this.commonMethodService.createCaptchaCarrerPage();
   }
 
+  controlLoginForm() {
+    this.loginForm = this.fb.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required],
+      captcha: ['']
+    })
+  }
+
+  clearForm() {
+    this.loginForm.reset();
+    this.controlLoginForm();
+  }
+
+  onClickLogin() {
+    let userId = this.loginForm.value.username;
+    let userPassword = this.loginForm.value.password;
+    if (this.loginForm.invalid) {
+      this.commonMethodService.matSnackBar('Please Enter Required Credentials', 1);
+      return;
+    }
+    else {
+      if (this.loginForm.value.captcha == this.commonMethodService.checkvalidateCaptcha()) {
+        // localStorage.setItem('user-data',JSON.stringify(this.loginForm.value));
+        // this.clearForm();
+        // alert('success');
+        // return
+        this.service.setHttp('get', 'whizhack_cms/login/CheckLogin/' + userId + '/' + userPassword, false, false, false, 'whizhackService');
+        this.service.getHttp().subscribe({
+          next: (res: any) => {
+            if (res.statusCode == '200') {
+              localStorage.setItem('user-data',JSON.stringify(res.responseData));
+              this.clearForm();
+              this.router.navigate(['/dashboard']);
+              this.commonMethodService.matSnackBar('Logged-in Successfuly', 0)
+              console.log(this.loginForm.value);
+            }
+            else{
+              this.commonMethodService.matSnackBar('The username or password, you entered is incorrect',1)
+            }
+          }
+        })
+      }
+      else {
+        this.commonMethodService.matSnackBar('Invalid Captcha !', 1)
+      }
+    }
+  }
 }
