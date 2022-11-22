296ec118b92da4f65adb5501b08046fa116b6e42
Vinayak-ShauryaTechnosoft <vinayak.bombale@shauryatechnosoft.com>
2022-11-22 15:03:14 +0530

    changes done

--- src/app/partial/enquiries/enquiries.component.ts
+++ src/app/partial/enquiries/enquiries.component.ts
@@ -12,18 +12,6 @@ export interface PeriodicElement {
   actions: string;
 }
 
-// const ELEMENT_DATA: PeriodicElement[] = [
-//   {srno: 1, name: 'Mr. Anil Vishwas', emailid: 'anilvishwas@gmail.com', contactno: '9876543210', course: 'CyberNinja', page:'Traning', actions:''},
-//   {srno: 2, name: 'Mr. Sunil Rajaram Shinde', emailid: 'sunilshinde@gmail.com', contactno: '7894651320', course: 'CyberNinja', page:'Traning', actions:''},
-//   {srno: 3, name: 'Ms. Shrutika Ravikiran Nalawade', emailid: 'shrutika@gmail.com', contactno: '8951320505', course: 'CyberNinja', page:'Traning', actions:''},
-//   {srno: 4, name: 'Mr. Anil Vishwas', emailid: 'anilvishwas@gmail.com', contactno: '9876543210', course: 'CyberNinja', page:'Traning', actions:''},
-//   {srno: 5, name: 'Mr. Anil Vishwas', emailid: 'anilvishwas@gmail.com', contactno: '9876543210', course: 'CyberNinja', page:'Traning', actions:''},
-//   {srno: 6, name: 'Mr. Anil Vishwas', emailid: 'anilvishwas@gmail.com', contactno: '9876543210', course: 'CyberNinja', page:'Traning', actions:''},
-//   {srno: 7, name: 'Mr. Anil Vishwas', emailid: 'anilvishwas@gmail.com', contactno: '9876543210', course: 'CyberNinja', page:'Traning', actions:''},
-//   {srno: 8, name: 'Mr. Anil Vishwas', emailid: 'anilvishwas@gmail.com', contactno: '9876543210', course: 'CyberNinja', page:'Traning', actions:''},
-//   {srno: 9, name: 'Mr. Anil Vishwas', emailid: 'anilvishwas@gmail.com', contactno: '9876543210', course: 'CyberNinja', page:'Traning', actions:''},
-//   {srno: 10, name: 'Mr. Anil Vishwas', emailid: 'anilvishwas@gmail.com', contactno: '9876543210', course: 'CyberNinja', page:'Traning', actions:''},
-// ];
 @Component({
   selector: 'app-enquiries',
   templateUrl: './enquiries.component.html',
@@ -55,6 +43,10 @@ export class EnquiriesComponent implements OnInit {
         }
       }),
     })
+  }
+
+  deleteUser(event?:any){
+    console.log(event);
   }
 }
 
