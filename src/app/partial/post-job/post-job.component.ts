94941ec18b5df14521990330a0ffffd2085da627
Shrutika-ShauryaTechnosoft <shrutika.mapari@shauryatechnosoft.com>
2022-11-22 14:24:42 +0530

    post-job ts upload

--- src/app/partial/post-job/post-job.component.ts
+++ src/app/partial/post-job/post-job.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import {MatDialog} from '@angular/material/dialog';
 import { JobDetailsComponent } from './job-details/job-details.component';
-
+import { FormGroup,FormBuilder, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { ApiService } from 'src/app/core/services/api.service';
+import {ErrorHandlerService} from 'src/app/core/services/error-handler.service';
 export interface PeriodicElement {
   title: string;
   srno: number;
@@ -31,9 +34,12 @@ const ELEMENT_DATA: PeriodicElement[] = [
   styleUrls: ['./post-job.component.css']
 })
 export class PostJobComponent implements OnInit {
+  postNewJobFrm!:FormGroup;
   displayedColumns: string[] = ['srno', 'title', 'location', 'postdate','lastdate','publish', 'actions'];
   dataSource = ELEMENT_DATA;
-  constructor(public dialog: MatDialog) { }
+  editFlag:boolean=false;
+  constructor(public dialog: MatDialog,private fb:FormBuilder,private snackbar:MatSnackBar,
+    private service:ApiService,private error:ErrorHandlerService) { }
 
   openDialog(): void {
     this.dialog.open(JobDetailsComponent,{
@@ -42,6 +48,120 @@ export class PostJobComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    this.formData();
+    // this.bindTable();
   }
+// ----------------------------Start Form Field Here-------------------------------
+  formData(){
+    this.postNewJobFrm = this.fb.group({
+      jobTitle: ['',Validators.required],
+      jobLocation: ['',Validators.required],
+      dateOfPosting: ['',Validators.required],
+      lastDateOfApp: ['',Validators.required],
+      description: ['',Validators.required],
+      roles: [''],
+      experience: [''],
+      qualification:[''],
+       skills:['']
+    });
+  }
+  // ----------------------------End Form Field Here-------------------------------
 
+  //---------------------------for Validation Handle---------------------------
+  get f() {
+    return this.postNewJobFrm.controls;
+  }
+  //---------------------------for Validation Handle---------------------------/
+
+  //----------------------------Start Bind Table Logic Here--------------------
+// bindTable(){
+//   this.service.setHttp('get','',false,false,false,'whizhackService');
+//   this.service.getHttp().subscribe({
+//     next:(res:any)=>{
+//       if(res.statusCode == '200'){
+//         this.dataSource=res.res.responseData;
+//       }
+//       else{
+//         this.dataSource=[];
+//       }
+//     },
+//     error:(error:any)=>{
+//       console.log("Error:",error);
+//       this.error.handelError(error.statusCode);
+//     }
+//   })
+// }
+//----------------------------End Bind Table Logic Here------------------------
+
+//----------------------------Start Publish Button Logic Here------------------
+// onClickToggle(element:any){ 
+//   this.service.setHttp('put','',false,false,false,'whizhackService');
+//   this.service.getHttp().subscribe({
+//     next: (res: any) =>{
+//       if(res.statusCode == '200')
+//       {
+//       this.bindTable();
+//       console.log("Toggle",element);   
+//       } 
+//     }
+//   })
+// }
+//----------------------------End Publish Button Logic Here------------------
+
+// ----------------------------Start Delete Logic Here-------------------------
+// onDelete(id:any){
+//   this.service.setHttp('delete',''+id,false,false,false,'whizhackService');
+//   this.service.getHttp().subscribe({
+//     next:(res:any)=>{
+//       if(res.statusCode == '200'){
+//         this.snackbar.open(res.statusMessage,'ok');
+//         this.bindTable();
+//       }
+//     },
+//     error:(error:any)=>{
+//       console.log("Error",error);
+//       this.error.handelError(error.statusCode);
+//     }
+//   })
+
+// }
+// ----------------------------End Delete Logic Here---------------------------
+
+// ----------------------------Start Submit Logic Here-------------------------
+  // onSubmit(){
+  //   let data=this.postNewJobFrm.value;
+  //   if(!this.editFlag){
+  //     this.service.setHttp('post','',false,data,false,'whizhackService');
+  //     this.service.getHttp().subscribe({
+  //       next:(res:any)=>{
+  //         if (res.statusCode == '200'){
+  //           this.snackbar.open(res.statusMessage, 'ok');
+  //           this.bindTable();
+  //         }
+  //       },
+  //       error:(error:any)=>{
+  //         console.log("Error:",error);
+  //         this.error.handelError(error.statusCode)
+  //       }
+  //     })
+  //   }
+  //   else{
+  //     this.editFlag=true;
+  //     this.service.setHttp('put','',false,data,false,'whizhackService');
+  //     this.service.getHttp().subscribe({
+  //       next:(res:any)=>{
+  //         if(res.statusCode == '200'){
+  //           this.snackbar.open(res.statusMessage,'ok');
+  //           this.bindTable();
+  //         }
+  //       },
+  //         error: (error: any) => {
+  //           console.log("Error : ", error);
+  //           this.error.handelError(error.statusCode);
+          
+  //       }
+  //     })
+  //   }
+  // }
+ // ----------------------------End Submit Logic Here-------------------------------
 }
