e798e455480948100b8bff70afb315ebf25c89b3
stpl-skishorUI <kishor.shetti@shauryatechnosoft.com>
2022-11-22 15:44:11 +0530

    new page components added as gallery master and media coverage

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,6 +14,7 @@ const routes: Routes = [
     component: PartialLayoutComponent,
     loadChildren: () => import('./partial/partial-layout/partial-layout.module').then(m => m.PartialLayoutModule)
   },
+  
 ]
 
 @NgModule({
---
+++ src/app/partial/masters/media-coverage/media-coverage.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-media-coverage',
+  templateUrl: './media-coverage.component.html',
+  styleUrls: ['./media-coverage.component.css']
+})
+export class MediaCoverageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
