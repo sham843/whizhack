73c3fa443252e4328047d4464285391a83d3b690
Shital Kirolkar <shital.kirolkar@shauryatechnosoft.com>
2022-11-22 14:10:09 +0530

    auth_guard_implementation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './core/guards/auth.guard';
+import { CheckLoggedInGuard } from './core/guards/check-logged-in.guard';
 import { PartialLayoutComponent } from './partial/partial-layout/partial-layout.component';
 import { WebLayoutComponent } from './web/web-layout/web-layout.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: '', component: WebLayoutComponent, loadChildren: () => import('./web/web-layout/web-layout.module').then(m => m.WebLayoutModule) },
+  { path: '', canActivate:[CheckLoggedInGuard], component: WebLayoutComponent, loadChildren: () => import('./web/web-layout/web-layout.module').then(m => m.WebLayoutModule) },
   {
     path: '',
-    canActivate: [],
+    canActivate: [AuthGuard],
     component: PartialLayoutComponent,
     loadChildren: () => import('./partial/partial-layout/partial-layout.module').then(m => m.PartialLayoutModule)
   },
