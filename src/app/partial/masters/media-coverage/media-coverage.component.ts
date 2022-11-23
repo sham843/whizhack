85c46156b727b2a9a511eb9571fbc706d6d6e736
Shital Kirolkar <shital.kirolkar@shauryatechnosoft.com>
2022-11-23 18:29:31 +0530

    media_coverage_api_changes

--- src/app/partial/masters/media-coverage/media-coverage.component.ts
+++ src/app/partial/masters/media-coverage/media-coverage.component.ts
@@ -1,8 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatDialog } from '@angular/material/dialog';
+import { NgxSpinnerService } from 'ngx-spinner';
 import { ApiService } from 'src/app/core/services/api.service';
+import { CommonMethodService } from 'src/app/core/services/common-method.service';
+import { ErrorHandlerService } from 'src/app/core/services/error-handler.service';
 import { FormValidationService } from 'src/app/core/services/form-validation.service';
+import { WebStorageService } from 'src/app/core/services/web-storage.service';
 import { ConfirmationModalComponent } from 'src/app/dialogs/confirmation-modal/confirmation-modal.component';
 @Component({
   selector: 'app-media-coverage',
@@ -16,14 +20,20 @@ export class MediaCoverageComponent implements OnInit {
   get f() { return this.frmMedia.controls };
   totalCount: number = 0;
   currentPage: number = 0;
+  pageSize: number = 10;
 
-  displayedColumns: string[] = ['srno', 'title', 'action'];
+  displayedColumns: string[] = ['srno', 'article_Title', 'source', 'url', 'action'];
   dataSource: any;
   
   constructor(private fb: FormBuilder, 
     public vs: FormValidationService,
     private api: ApiService,
-    public dialog: MatDialog) { }
+    public dialog: MatDialog,
+    private spinner: NgxSpinnerService,
+    public commonMethod: CommonMethodService,
+    public error: ErrorHandlerService,
+    public webStorageService: WebStorageService,
+    public apiService: ApiService) { }
 
   ngOnInit(): void {
     this.createMediaForm();
@@ -44,15 +54,55 @@ export class MediaCoverageComponent implements OnInit {
     this.getMediaList();
   }
 
-  getMediaList(){
+  getQueryString() {
+    let str = "?pageno=" + this.currentPage + "&pagesize=" + this.pageSize;
+    return str;
+  }
 
+  getMediaList(){
+    this.spinner.show();
+    this.apiService.setHttp('get', "whizhack_cms/media/GetAllByPagination" + this.getQueryString(), false, false, false, 'whizhackService');
+    this.apiService.getHttp().subscribe({
+      next: (res: any) => {
+        if (res.statusCode == 200) {
+          this.dataSource = res.responseData?.responseData1;
+          this.totalCount = res.responseData?.responseData2?.totalCount;
+          this.currentPage = res.responseData?.responseData2?.pageNo;
+        } else {
+          this.dataSource = [];
+          this.totalCount = 0;
+          this.commonMethod.checkDataType(res.statusMessage) == false ? this.error.handelError(res.statusCode) : this.commonMethod.matSnackBar(res.statusMessage, 1);
+        }
+        this.spinner.hide();
+      },
+      error: ((error: any) => { this.error.handelError(error.status); this.spinner.hide(); })
+    })
   }
 
   onMediaSubmit(){
     if(this.frmMedia.invalid){
       return;
     }else{
+      var req = {
+        ...this.webStorageService.createdByProps(),
+        ...this.frmMedia.value
+      }
 
+      this.apiService.setHttp((this.submitBtnTxt = 'Update' ? 'put' : 'post'), "whizhack_cms/media/" + (this.submitBtnTxt = 'Update' ? 'Update' : 'Register'), false, req, false, 'WBMiningService');
+      this.apiService.getHttp().subscribe({
+        next: (res: any) => {
+          if (res.statusCode == 200) {
+            this.spinner.hide();
+            this.getMediaList();
+            this.clearMediaForm();
+            this.commonMethod.checkDataType(res.statusMessage) == false ? this.error.handelError(res.statusCode) : this.commonMethod.matSnackBar(res.statusMessage, 0);
+          } else {
+            this.spinner.hide();
+            this.commonMethod.checkDataType(res.statusMessage) == false ? this.error.handelError(res.statusCode) : this.commonMethod.matSnackBar(res.statusMessage, 1);
+          }
+        },
+        error: ((error: any) => { this.error.handelError(error.status); this.spinner.hide(); })
+      })
     }
   }
 
@@ -86,11 +136,12 @@ export class MediaCoverageComponent implements OnInit {
           "modifiedBy": 0,
         }
     
-        this.api.setHttp('delete', 'whizhack_cms/course/Delete', false, deleteObj, false, 'whizhackService');
+        this.api.setHttp('delete', 'whizhack_cms/media/Delete', false, deleteObj, false, 'whizhackService');
         this.api.getHttp().subscribe({
           next: ((res: any) => {
-            if (res.statusCode === '200') {
+            if (res.statusCode === 200) {
               this.getMediaList();
+              this.clearMediaForm();
             }
           }),
           error: (error: any) => {
