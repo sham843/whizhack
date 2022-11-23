4b2f6f1a9009e2f0f6e1843f3d32e508cf899f7b
Shrutika-ShauryaTechnosoft <shrutika.mapari@shauryatechnosoft.com>
2022-11-23 09:52:28 +0530

    Bind Table api upl

--- src/app/partial/post-job/post-job.component.ts
+++ src/app/partial/post-job/post-job.component.ts
@@ -4,8 +4,8 @@ import { Editor, Toolbar } from 'ngx-editor';
 import { JobDetailsComponent } from './job-details/job-details.component';
 import { FormGroup,FormBuilder, Validators } from '@angular/forms';
 // import { MatSnackBar } from '@angular/material/snack-bar';
-// import { ApiService } from 'src/app/core/services/api.service';
-// import {ErrorHandlerService} from 'src/app/core/services/error-handler.service';
+ import { ApiService } from 'src/app/core/services/api.service';
+ import {ErrorHandlerService} from 'src/app/core/services/error-handler.service';
 
 export interface PeriodicElement {
   title: string;
@@ -17,18 +17,18 @@ export interface PeriodicElement {
   actions: string;
 }
 
-const ELEMENT_DATA: PeriodicElement[] = [
-  {srno: 1, title: 'Hydrogen', location: '1.0079', postdate: 'H', lastdate: 'H',publish:'',actions:''},
-  {srno: 2, title: 'Helium', location: '4.0026', postdate: 'He', lastdate: 'H',publish:'',actions:''},
-  {srno: 3, title: 'Lithium', location: '6.941', postdate: 'Li', lastdate: 'H',publish:'',actions:''},
-  {srno: 4, title: 'Beryllium', location: '9.0122', postdate: 'Be', lastdate: 'H',publish:'',actions:''},
-  {srno: 5, title: 'Boron', location: '10.811', postdate: 'B', lastdate: 'H',publish:'',actions:''},
-  {srno: 6, title: 'Carbon', location: '12.0107', postdate: 'C', lastdate: 'H',publish:'',actions:''},
-  {srno: 7, title: 'Nitrogen', location: '14.0067', postdate: 'N', lastdate: 'H',publish:'',actions:''},
-  {srno: 8, title: 'Oxygen', location: '15.9994', postdate: 'O', lastdate: 'H',publish:'',actions:''},
-  {srno: 9, title: 'Fluorine', location: '18.9984', postdate: 'F', lastdate: 'H',publish:'',actions:''},
-  {srno: 10, title: 'Neon', location: '20.1797', postdate: 'Ne', lastdate: 'H',publish:'',actions:''},
-];
+// const ELEMENT_DATA: PeriodicElement[] = [
+//   {srno: 1, title: 'Hydrogen', location: '1.0079', postdate: 'H', lastdate: 'H',publish:'',actions:''},
+//   {srno: 2, title: 'Helium', location: '4.0026', postdate: 'He', lastdate: 'H',publish:'',actions:''},
+//   {srno: 3, title: 'Lithium', location: '6.941', postdate: 'Li', lastdate: 'H',publish:'',actions:''},
+//   {srno: 4, title: 'Beryllium', location: '9.0122', postdate: 'Be', lastdate: 'H',publish:'',actions:''},
+//   {srno: 5, title: 'Boron', location: '10.811', postdate: 'B', lastdate: 'H',publish:'',actions:''},
+//   {srno: 6, title: 'Carbon', location: '12.0107', postdate: 'C', lastdate: 'H',publish:'',actions:''},
+//   {srno: 7, title: 'Nitrogen', location: '14.0067', postdate: 'N', lastdate: 'H',publish:'',actions:''},
+//   {srno: 8, title: 'Oxygen', location: '15.9994', postdate: 'O', lastdate: 'H',publish:'',actions:''},
+//   {srno: 9, title: 'Fluorine', location: '18.9984', postdate: 'F', lastdate: 'H',publish:'',actions:''},
+//   {srno: 10, title: 'Neon', location: '20.1797', postdate: 'Ne', lastdate: 'H',publish:'',actions:''},
+// ];
 
 @Component({
   selector: 'app-post-job',
@@ -37,13 +37,14 @@ const ELEMENT_DATA: PeriodicElement[] = [
 })
 export class PostJobComponent implements OnInit {
   postNewJobFrm!:FormGroup;
-  displayedColumns: string[] = ['srno', 'title', 'location', 'postdate','lastdate','publish', 'actions'];
-  dataSource = ELEMENT_DATA;
+  displayedColumns: string[] = ['srno', 'title', 'location', 'postdate','lastdate','actions'];
+  // 'publish',
+  dataSource = new Array();
   editFlag:boolean=false;
-  constructor(public dialog: MatDialog,private fb:FormBuilder
+  constructor(public dialog: MatDialog,private fb:FormBuilder,
     // private snackbar:MatSnackBar,
-    // private service:ApiService,
-    // private error:ErrorHandlerService
+    private service:ApiService,
+     private error:ErrorHandlerService
     ) { }
 
   editorRoles!: Editor;
@@ -65,6 +66,7 @@ export class PostJobComponent implements OnInit {
 
   ngOnInit(): void {
     this.formData();
+    this.bindTable();
     this.editorRoles = new Editor();
     this.editorExperience = new Editor();
     this.editorQualification = new Editor();
@@ -93,23 +95,24 @@ export class PostJobComponent implements OnInit {
   //---------------------------for Validation Handle---------------------------/
 
   //----------------------------Start Bind Table Logic Here--------------------
-// bindTable(){
-//   this.service.setHttp('get','',false,false,false,'whizhackService');
-//   this.service.getHttp().subscribe({
-//     next:(res:any)=>{
-//       if(res.statusCode == '200'){
-//         this.dataSource=res.res.responseData;
-//       }
-//       else{
-//         this.dataSource=[];
-//       }
-//     },
-//     error:(error:any)=>{
-//       console.log("Error:",error);
-//       this.error.handelError(error.statusCode);
-//     }
-//   })
-// }
+  // http://whizhackwebapi.mahamining.com/whizhack_cms/postjobs/GetAllCourses?pageno=1&pagesize=10
+bindTable(){
+  this.service.setHttp('get','whizhack_cms/postjobs/GetAllCourses?pageno=1&pagesize=10',false,false,false,'whizhackService');
+  this.service.getHttp().subscribe({
+    next:(res:any)=>{
+      if(res.statusCode == '200'){
+        this.dataSource=res.responseData;
+      }
+      else{
+        this.dataSource=[];
+      }
+    },
+    error:(error:any)=>{
+      console.log("Error:",error);
+      this.error.handelError(error.statusCode);
+    }
+  })
+}
 //----------------------------End Bind Table Logic Here------------------------
 openDialog1(obj?: any): void {
   this.dialog.open(JobDetailsComponent,{
