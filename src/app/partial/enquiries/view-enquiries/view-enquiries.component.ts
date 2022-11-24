7288580732ce0260a7b797358e0d99908d168341
Vinayak-ShauryaTechnosoft <vinayak.bombale@shauryatechnosoft.com>
2022-11-24 09:42:16 +0530

    small changes

--- src/app/partial/enquiries/view-enquiries/view-enquiries.component.ts
+++ src/app/partial/enquiries/view-enquiries/view-enquiries.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, Inject, OnInit } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { CommonMethodService } from 'src/app/core/services/common-method.service';
@@ -9,18 +10,32 @@ import { CommonMethodService } from 'src/app/core/services/common-method.service
 })
 export class ViewEnquiriesComponent implements OnInit {
 
-  constructor(public dialogRef: MatDialogRef<ViewEnquiriesComponent>, @Inject(MAT_DIALOG_DATA) public data: any, private common: CommonMethodService) { }
+  constructor(public dialogRef: MatDialogRef<ViewEnquiriesComponent>, @Inject(MAT_DIALOG_DATA) public data: any, private common:CommonMethodService,
+  private http:HttpClient) { }
   newArray = [this.data];
   deviceDataarray=new Array();
+  deviceIpAddress:any;
+  ipAddress:any;
+  address:any
 
   ngOnInit(): void {
-    this.getDevice()
+    this.getDevice();
+    this.getdeviceIpAddress();
   }
 
 
   getDevice() {
    this.common.getDeviceInfo()
     this.deviceDataarray=[this.common]
+  }
+
+  getdeviceIpAddress(){
+    this.http.get("https://api.ipify.org/?format=json").subscribe((res: any) => {
+    this.deviceIpAddress=res 
+     let arr=JSON.stringify(this.deviceIpAddress)
+     console.log('type',typeof(arr));
+     this.address=arr.slice(7,22);
+    })   
   }
 
   
