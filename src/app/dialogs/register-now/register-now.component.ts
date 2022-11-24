4829ed0faf101dbac2f0024c11a6e32cab1cce9e
Vinayak-ShauryaTechnosoft <vinayak.bombale@shauryatechnosoft.com>
2022-11-24 10:29:03 +0530

    all console remove

--- src/app/dialogs/register-now/register-now.component.ts
+++ src/app/dialogs/register-now/register-now.component.ts
@@ -13,12 +13,11 @@ import { Router } from '@angular/router';
 export class RegisterNowComponent implements OnInit {
   registerForm!: FormGroup;
 
-
   constructor(public dialogRef: MatDialogRef<RegisterNowComponent>, @Inject(MAT_DIALOG_DATA) public data: any,
     private fb: FormBuilder, private service: ApiService,
     private errorSer: ErrorHandlerService,
     public validator: FormValidationService,
-    private route:Router) { }
+    private route: Router) { }
 
   ngOnInit(): void {
     this.getFormData();
@@ -30,20 +29,19 @@ export class RegisterNowComponent implements OnInit {
       fullName: ['', [Validators.required, Validators.maxLength(30)]],
       email: ['', [Validators.required, Validators.pattern('^[a-zA-Z0-9._%+-]+@([a-z0-9.-]+[.])+[a-z]{2,5}$')]],
       mobileNo: ['', [Validators.required, Validators.pattern('[7-9]\\d{9}'), Validators.maxLength(10)]],
-      courseId:[this.data == 1 ? 'Cyber Ninja' : this.data == 2 ? 'Cyber Samurai' : 'Cyber Guru'],
+      courseId: [this.data == 1 ? 'Cyber Ninja' : this.data == 2 ? 'Cyber Samurai' : 'Cyber Guru'],
       message: ['', [Validators.required]],
     })
   }
 
   onSubmit() {
-    // debugger
     if (this.registerForm.invalid) {
       return
     } else {
       let formData = this.registerForm.value;
-      formData.pageName=this.route.url || '';
-      formData.courseId=this.data == 'Cyber Ninja' ? 1 : formData.courseId=this.data == 'Cyber Samurai' ? 2 : 3;
-      console.log( 'cid',formData.courseId);
+      formData.pageName = this.route.url || '';
+      formData.courseId = this.data == 'Cyber Ninja' ? 1 : formData.courseId = this.data == 'Cyber Samurai' ? 2 : 3;
+      console.log('cid', formData.courseId);
       this.service.setHttp('post', 'whizhack_cms/register/Register', false, formData, false, 'whizhackService');
       this.service.getHttp().subscribe({
         next: ((res: any) => {
@@ -55,12 +53,6 @@ export class RegisterNowComponent implements OnInit {
           this.errorSer.handelError(error.status);
         }
       })
-      console.log('fdata',formData);
     }
-
-    
   }
-
-
-
 }
