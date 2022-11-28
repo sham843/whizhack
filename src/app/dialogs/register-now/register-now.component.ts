0541004027a6be55204a02f7318e87f9e6cbaf95
Vinayak-ShauryaTechnosoft <vinayak.bombale@shauryatechnosoft.com>
2022-11-28 12:45:21 +0530

    done

--- src/app/dialogs/register-now/register-now.component.ts
+++ src/app/dialogs/register-now/register-now.component.ts
@@ -32,7 +32,7 @@ export class RegisterNowComponent implements OnInit {
   getFormData() {
     this.registerForm = this.fb.group({
       fullName: ['', [Validators.required,Validators.maxLength(50)]],
-      email: ['', [Validators.required,Validators.email]],
+      email: ['', [Validators.required,Validators.pattern('^[a-zA-Z0-9._%+-]+@([a-z0-9.-]+[.])+[a-z]{2,5}$')]],
       mobileNo: ['', [Validators.required, Validators.pattern('[6-9]\\d{9}')]],
       courseId: [this.data.course_Title],
       message: ['', [Validators.required,Validators.maxLength(500)]]
