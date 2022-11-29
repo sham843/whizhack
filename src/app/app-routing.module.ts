1cd053ed5fb89550484a54c8d0c995d1e439b477
vaishaliborate <vaishali.borate@shauryatechnosoft.com>
2022-11-29 16:09:11 +0530

    add titles

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthGuard } from './core/guards/auth.guard';
 import { CheckLoggedInGuard } from './core/guards/check-logged-in.guard';
+import { PageNotFoundComponent } from './errors/page-not-found/page-not-found.component';
 import { PartialLayoutComponent } from './partial/partial-layout/partial-layout.component';
 import { WebLayoutComponent } from './web/web-layout/web-layout.component';
 
@@ -15,7 +16,7 @@ const routes: Routes = [
     loadChildren: () => import('./partial/partial-layout/partial-layout.module').then(m => m.PartialLayoutModule)
   },
   { path: 'forgot-password', loadChildren: () => import('./web/forgot-password/forgot-password.module').then(m => m.ForgotPasswordModule) },
-  
+  {path:'**',component:PageNotFoundComponent}
   
 ]
 
