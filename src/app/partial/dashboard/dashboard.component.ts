7bd665d117d5d4837c8b11af200bcad98cc53d45
anandramekar-stpl <anand.ramekar@shauryatechnosoft.com>
2022-12-02 15:33:05 +0530

    dashboardData binding done

--- src/app/partial/dashboard/dashboard.component.ts
+++ src/app/partial/dashboard/dashboard.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from 'src/app/core/services/api.service';
+import { CommonMethodService } from 'src/app/core/services/common-method.service';
+import { ErrorHandlerService } from 'src/app/core/services/error-handler.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,10 +9,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
+  dashboardData!:any;
 
-  constructor() { }
+  constructor(private service: ApiService,private commonMethod: CommonMethodService, private errorHandler: ErrorHandlerService,) {}
 
   ngOnInit(): void {
+    this.displayDashboardData();
   }
 
+  displayDashboardData() {
+    this.service.setHttp('get', 'whizhack_cms/Dashboard/getbydashboard', false, false, false, 'whizhackService');
+    this.service.getHttp().subscribe({
+      next: (res: any) => {
+        if (res.statusCode == '200') {
+          this.dashboardData = res.responseData;
+        }
+      }, error: (error: any) => {
+        this.commonMethod.checkDataType(error.statusText) == false ? this.errorHandler.handelError(error.statusCode) : this.commonMethod.matSnackBar(error.statusText, 1);
+      }
+    })
+  }
 }
