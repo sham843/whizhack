757effb2487787f34f4d4037e081611097531779
stpl-skishorUI <kishor.shetti@shauryatechnosoft.com>
2022-12-02 14:18:56 +0530

    bootcamp registration page designed

--- src/app/web/training/bootcampt/bootcamp-registration/bootcamp-registration.component.ts
+++ src/app/web/training/bootcampt/bootcamp-registration/bootcamp-registration.component.ts
@@ -1,17 +1,34 @@
 import {Component} from '@angular/core';
 import {FormBuilder, Validators} from '@angular/forms';
 
+interface Food {
+  value: string;
+  viewValue: string;
+}
+
 @Component({
   selector: 'app-bootcamp-registration',
   templateUrl: './bootcamp-registration.component.html',
   styleUrls: ['./bootcamp-registration.component.css']
 })
 export class BootcampRegistrationComponent {
+  foods: Food[] = [
+    {value: 'steak-0', viewValue: 'Steak'},
+    {value: 'pizza-1', viewValue: 'Pizza'},
+    {value: 'tacos-2', viewValue: 'Tacos'},
+  ];
+
   firstFormGroup = this._formBuilder.group({
     firstCtrl: ['', Validators.required],
   });
   secondFormGroup = this._formBuilder.group({
     secondCtrl: ['', Validators.required],
+  });
+  thirdFormGroup = this._formBuilder.group({
+    thirdCtrl: ['', Validators.required],
+  });
+  fourthFormGroup = this._formBuilder.group({
+    fourthCtrl: ['', Validators.required],
   });
   isLinear = false;
 
