20233bd41e060ef136d0399db79d2503269a47b0
Vinayak-ShauryaTechnosoft <vinayak.bombale@shauryatechnosoft.com>
2022-12-08 12:46:53 +0530

    email validation updated

--- src/app/dialogs/register-now/register-now.component.ts
+++ src/app/dialogs/register-now/register-now.component.ts
@@ -32,7 +32,7 @@ export class RegisterNowComponent implements OnInit {
   getFormData() {
     this.registerForm = this.fb.group({
       fullName: ['', [Validators.required, Validators.maxLength(50),Validators.pattern('^[a-zA-Z][a-zA-Z\\s]+$')]],
-      email: ['', [Validators.required,Validators.email, Validators.pattern(this.validator.valEmailId)]],
+      email: ['', [Validators.required,Validators.email,Validators.email]],
       mobileNo: ['', [Validators.required,Validators.maxLength(16),Validators.pattern('(^[0-9\)\(+-\\s]{5,16})*[^\s]$')]],
       courseId: [this.data.course_Title],
       message: ['', [Validators.required, Validators.maxLength(500)]],
