8d73c4a485fd08c56793767ed44056a8caed5011
Shital Kirolkar <shital.kirolkar@shauryatechnosoft.com>
2022-12-09 11:23:50 +0530

    Carousel_changes_for_home_page

--- src/app/web/home/home.component.ts
+++ src/app/web/home/home.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+declare var bootstrap: any;
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, AfterViewInit {
 
   constructor() { }
 
   ngOnInit(): void {
+
+  }
+
+  ngAfterViewInit(): void {
+    const myCarouselElement = document.querySelector('#homecarousel')
+    const carousel = new bootstrap.Carousel(myCarouselElement, {
+      interval: 2000,
+      wrap: false
+    })
+    console.log(carousel);
+    
   }
 
 }
